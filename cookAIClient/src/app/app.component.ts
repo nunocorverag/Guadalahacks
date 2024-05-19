2da61db12cd9413fb1044a397ea666ebc1a3d735
Edgarong17 <ngedgaro@gmail.com>
2024-05-18 20:37:23 -0600

    X

--- cookAIClient/src/app/app.component.ts
+++ cookAIClient/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild("Reguisterform") Reguisterform:any;
   title = 'cookAIClient';
+  reguister(){
+    let name= this.Reguisterform.controls.name.value;
+    let email= this.Reguisterform.controls.email.value;
+    let password= this.Reguisterform.controls.password.value;
+    let confirm= this.Reguisterform.controls.confirm.value;
+    console.log("Sirvo")
+  }
 }
