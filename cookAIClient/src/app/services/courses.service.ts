8ca5c429effef4502b03145efac5bdf5f6e46091
Th3-Nevazzz <162382572+Th3-Nevazzz@users.noreply.github.com>
2024-05-18 23:34:19 -0600

    courses

---
+++ cookAIClient/src/app/services/courses.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Constantes } from '../classes/constantes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AccessService } from './access.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+  token:any = "";
+
+  constructor(private http : HttpClient, private authSrv: AccessService) {
+  if(this.authSrv.getSession()){
+    this.token=JSON.parse(this.authSrv.getSession());
+  }
+   }
+   public obtenerCursos(): any {
+    let link: string = Constantes.CAKE + "api/getmytopics";
+    let api_key:any = this.token.token;
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${api_key}`);
+    return this.http.get(link, {headers});
+   }
+}
