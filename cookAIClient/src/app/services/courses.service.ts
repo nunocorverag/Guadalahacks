c8fab28aee41a03147a8b4bbc575b325441033a3
Gustavo Alexander Nuño Corvera <gnuno2003@gmail.com>
2024-05-18 23:40:49 -0600

    sijalabue

--- cookAIClient/src/app/services/courses.service.ts
+++ cookAIClient/src/app/services/courses.service.ts
@@ -15,9 +15,9 @@ export class CoursesService {
   }
    }
    public obtenerCursos(): any {
-    let link: string = Constantes.CAKE + "api/getmytopics";
-    let api_key:any = this.token.token;
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${api_key}`);
+    let link: string = Constantes.CAKE + "api/getMyTopics";
+    // let api_key:any = this.token.token;
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOjIsImV4cCI6MTcxNjE2OTQ2N30.JIuaN6I80Atc71JfkNX4h1YEH_fdf0wScwRxMVGDF7elq9aG6NJaqy9affi93GzEqWf8ekRfxqBq8R6CNTvmH34XRtLP5WYr7VWO1oseQaQeK_wmeMge60-lv-K-jr0vtEm654rBEtw09aCZ2a51fks7o7bggcphx-bQ_seHYv4"}`);
     return this.http.get(link, {headers});
    }
 }
