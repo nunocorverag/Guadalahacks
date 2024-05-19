e1d109480925a71b1ada6fdd1305eaa4391e7912
Blitz <jsnajera1017@gmail.com>
2024-05-18 21:39:23 -0600

    Login created

--- cookAIClient/src/app/services/user.service.ts
+++ cookAIClient/src/app/services/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AccessService } from './access.service'; 
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  constructor(private http: HttpClient, private authSrv: AccessService) { }
+  
+  validateCredentials(email: string, password: string) {
+      return this.http.post<any>('/api/validate', { email, password });
+  }
 }
